691b55cb64f98b5aaa1264421df2117256dd25cf
sellyns00 <153070259+sellyns00@users.noreply.github.com>
2024-02-21 23:25:13 +0700

    Update CapstoneProject1_Selly.py

--- CapstoneProject1_Selly.py
+++ CapstoneProject1_Selly.py
@@ -146,60 +146,7 @@ while True :
         sewaMobil() 
     elif(pilihanMenu == '6') :
         exit()       
-    #     print('Daftar Mobil\n')
-    #     print('Index\t| Merk  \t\t| Tahun\t| Jenis\t| Status\t| Harga \t')
-    #     for i in range(len(listMobil)) :
-    #         print('{}\t| {}\t\t| {}\t| {}\t| {}    \t| {}\t'.format(i,listMobil[i]['Merk'],listMobil[i]['Tahun'],listMobil[i]['Jenis'],listMobil[i]['Status'],listMobil[i]['harga']))
-    #     indexMobil = int(input('Masukkan index Mobil yang ingin dihapus : '))
-    #     del listMobil[indexMobil]
-    #     print('Daftar Mobil\n')
-    #     print('Index\t| Merk  \t\t| Tahun\t| Jenis\t| Status\t| Harga \t')
-    #     for i in range(len(listMobil)) :
-    #         print('{}\t| {}\t\t| {}\t| {}\t| {}    \t| {}\t'.format(i,listMobil[i]['Merk'],listMobil[i]['Tahun'],listMobil[i]['Jenis'],listMobil[i]['Status'],listMobil[i]['harga']))
 
-    # elif(pilihanMenu == '5') :
-
-    #     print('Daftar Mobil\n')
-    #     print('Index\t| Merk  \t\t| Tahun\t| Jenis\t| Status\t| Harga \t')
-    #     for i in range(len(listMobil)) :
-    #         print('{}\t| {}\t\t| {}\t| {}\t| {}    \t| {}\t'.format(i,listMobil[i]['Merk'],listMobil[i]['Tahun'],listMobil[i]['Jenis'],listMobil[i]['Status'],listMobil[i]['harga']))
-    #     nama=str(input('Masukkan Nama Anda: '))
-    #     while True :
-    #         i = int(input('Masukkan index Mobil yang ingin disewa : '))
-    #         if i in range(len(listMobil)) and listMobil[i]['Status'] == 'Ada':
-    #             sewa = int(input('Berapa lama Anda ingin menyewa mobil? ... Hari :  '))
-    #             total = sewa * listMobil[i]['harga']
-    #             status = 'Berhasil disewa oleh' +' '+ nama
-    #             listMobil[i]['Status'] = status
-    #             cart.append([nama,listMobil[i]['Merk'],listMobil[i]['Tahun'],listMobil[i]['Jenis'],listMobil[i]['harga'],sewa,total,status])
-            
-    #         else :
-    #             print ('Maaf Mobil yang Anda pilih tidak tersedia\n')    
-                
-    #         print('Isi Cart Sewa\n')
-    #         print('Nama Customer\t| Merk\t\t| Tahun\t | Jenis | Harga\t| Lama Sewa (hari)\t | Total\t | Status')
-    #         for item in cart :
-    #             print(f'{item[0]}\t\t| {item[1]}\t| {item[2]}\t | {item[3]}\t | {item[4]}\t| {item[5]}\t\t\t | {item[6]}\t | {item[7]}\t')
-    #         checker = input('Mau sewa yang lain? (ya/tidak) = ')
-    #         if(checker == 'tidak') :
-    #             break
-
-    #     while True :
-    #         print('Total Yang Harus Dibayar = {}'.format(total))
-    #         jmlUang = int(input('Masukkan jumlah uang = '))
-    #         if(jmlUang > total) :
-    #             kembali = jmlUang - total
-    #             print('Terima kasih \n\nUang kembali anda = {}'.format(kembali))
-    #             cart.clear()
-    #             break
-    #         elif(jmlUang == total) :
-    #             print('Terima kasih')
-    #             cart.clear()
-    #             exit()
-    #         else :
-    #             kekurangan = total - jmlUang
-    #             print('Uang anda kurang sebesar {}'.format(kekurangan))
-    #             exit()
                 
     # elif(pilihanMenu == '6') :
     #     break
